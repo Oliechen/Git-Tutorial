235197bb9d7be45118dfcc016ab71ff3961945b5
Oliechen <wogada@gmail.com>
2018-04-27 01:05:43 +0800

    Add message string and print to display

--- Git Tutorial/ViewController.swift
+++ Git Tutorial/ViewController.swift
@@ -11,7 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+
+
+        let message = "Hello Git!"
+
+        print(message)
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
